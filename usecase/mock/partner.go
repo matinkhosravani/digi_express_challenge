5f7d416f8810b07068005510e37e353a9b024e78
Matin khosravani <matinkhosravani@gmail.com>
2023-06-29 14:48:03 +0330

    add test for storing partner

---
+++ usecase/mock/partner.go
@@ -0,0 +1,29 @@
+package mock
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/matinkhosravani/digi_express_challenge/domain"
+)
+
+type PartnerStoreUsecase struct {
+	ValidationFn func(c *gin.Context) (*domain.Partner, error)
+	StoreFn      func(partner *domain.Partner) error
+}
+
+func (uc *PartnerStoreUsecase) Validation(c *gin.Context) (*domain.Partner, error) {
+	if uc != nil && uc.ValidationFn != nil {
+		return uc.ValidationFn(c)
+	}
+
+	var p domain.Partner
+	err := c.BindJSON(&p)
+	return &p, err
+}
+
+func (uc *PartnerStoreUsecase) Store(partner *domain.Partner) error {
+	if uc != nil && uc.StoreFn != nil {
+		return uc.StoreFn(partner)
+	}
+
+	return nil
+}
