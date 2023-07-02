1d4284aaf67ed46eef8bb72bad41aad032394c54
Matin khosravani <matinkhosravani@gmail.com>
2023-07-02 16:50:37 +0330

    update init file

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ source_code_path="$(pwd)"
 # Step 1: Copy the .env.example file
 echo "Step 2: Copying .env.example..."
 cp ${source_code_path}/env.example ${source_code_path}/.env
+cp ${source_code_path}/testing.env.example ${source_code_path}/testing.env
 
 # Step 2: Prompt for database name and root password
 echo "Step 3: Setting up the database..."
@@ -30,13 +31,19 @@ docker-compose exec mysql mysql -uroot -p${rootpwd} -e "CREATE DATABASE IF NOT E
 # Step 5: Update .env file in Go source code
 echo "Step 6: Updating .env file in Go source code..."
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' "s/DB_NAME=.*/DB_NAME=${dbname}/" $source_code_path/.env
+    sed -i '' "s/DB_NAME=.*/DB_NAME=${dbname}/" $source_code_path/.env 
     sed -i '' "s/DB_PASS=.*/DB_PASS=${rootpwd}/" $source_code_path/.env
     sed -i '' "s/MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=${rootpwd}/" $source_code_path/.env
+    sed -i '' "s/DB_NAME=.*/DB_NAME=${dbname}_test/" $source_code_path/testing.env 
+    sed -i '' "s/DB_PASS=.*/DB_PASS=${rootpwd}/" $source_code_path/testing.env
+    sed -i '' "s/MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=${rootpwd}/" $source_code_path/testing.env
 else
     sed -i "s/DB_PASS=.*/DB_PASS=${rootpwd}/" $source_code_path/.env
     sed -i "s/DB_NAME=.*/DB_NAME=${dbname}/" $source_code_path/.env
     sed -i "s/MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=${rootpwd}/" $source_code_path/.env
+    sed -i "s/DB_PASS=.*/DB_PASS=${rootpwd}/" $source_code_path/testing.env
+    sed -i "s/DB_NAME=.*/DB_NAME=${dbname}_test/" $source_code_path/testing.env
+    sed -i "s/MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=${rootpwd}/" $source_code_path/testing.env
 fi
 
 # Step 6: dockeer-compose up
